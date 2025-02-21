e33b0cb90be0848caad10b7aa0fd2d3634ef9dda
caonguyen21 <devncnguyen@gmail.com>
2025-02-21 10:49:58 +0700

    #

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -59,7 +59,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   return (
     <div className="pb-40">
       <Cover preview url={document.coverImage} />
-      <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
+      <div className="mx-auto">
         <Toolbar preview initialData={document} />
         <Editor
           editable={false}
